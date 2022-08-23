95e4183ddf2d4c5e8241e5b38b2051621d5231ce
deyki <deikioveca@gmail.com>
2022-08-23 17:31:04 +0300

    New binding model

---
+++ src/main/java/deyki/FastTrade/domain/bindingModels/user/NewEmailBindingModel.java
@@ -0,0 +1,9 @@
+package deyki.FastTrade.domain.bindingModels.user;
+
+import lombok.Data;
+
+@Data
+public class NewEmailBindingModel {
+
+    private String email;
+}
---
+++ src/main/java/deyki/FastTrade/domain/bindingModels/user/NewPhoneNumberBindingModel.java
@@ -0,0 +1,9 @@
+package deyki.FastTrade.domain.bindingModels.user;
+
+import lombok.Data;
+
+@Data
+public class NewPhoneNumberBindingModel {
+
+    private Integer phoneNumber;
+}
