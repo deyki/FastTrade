ef24e7e9d00851a317f9cf9da24b151fde699bcb
deyki <deikioveca@gmail.com>
2022-08-23 17:41:56 +0300

    New binding model

---
+++ src/main/java/deyki/FastTrade/domain/bindingModels/user/NewUserNamesBindingModel.java
@@ -0,0 +1,11 @@
+package deyki.FastTrade.domain.bindingModels.user;
+
+import lombok.Data;
+
+@Data
+public class NewUserNamesBindingModel {
+
+    private String firstName;
+    private String middleName;
+    private String lastName;
+}
