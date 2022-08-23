07211c178c0a3e635085413c9c72b02c235ef68a
deyki <deikioveca@gmail.com>
2022-08-23 09:45:43 +0300

    Bean configurations

---
+++ src/main/java/deyki/FastTrade/config/BeanConfigs.java
@@ -0,0 +1,20 @@
+package deyki.FastTrade.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BeanConfigs {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
