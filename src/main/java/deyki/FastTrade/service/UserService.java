8e1c37dd1701aa849ed39b2e53680ee1248dec79
deyki <deikioveca@gmail.com>
2022-08-23 10:13:14 +0300

    Implemented signIn, signUp and changeUsernameById methods

--- src/main/java/deyki/FastTrade/service/UserService.java
+++ src/main/java/deyki/FastTrade/service/UserService.java
@@ -1,5 +1,6 @@
 package deyki.FastTrade.service;
 
+import deyki.FastTrade.domain.bindingModels.NewUsernameBindingModel;
 import deyki.FastTrade.domain.bindingModels.UserBindingModel;
 import deyki.FastTrade.domain.responseModels.SignInResponseModel;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,4 +10,6 @@ public interface UserService extends UserDetailsService {
     void signUp(UserBindingModel userBindingModel);
 
     SignInResponseModel signIn(UserBindingModel userBindingModel);
+
+    void changeUsernameById(Long userId, NewUsernameBindingModel newUsernameBindingModel);
 }
--- src/main/java/deyki/FastTrade/service/impl/UserServiceImpl.java
+++ src/main/java/deyki/FastTrade/service/impl/UserServiceImpl.java
@@ -1,36 +1,96 @@
 package deyki.FastTrade.service.impl;
 
+import deyki.FastTrade.domain.bindingModels.NewUsernameBindingModel;
 import deyki.FastTrade.domain.bindingModels.UserBindingModel;
+import deyki.FastTrade.domain.entity.User;
 import deyki.FastTrade.domain.responseModels.SignInResponseModel;
 import deyki.FastTrade.repository.UserRepository;
+import deyki.FastTrade.security.JWTUtil;
 import deyki.FastTrade.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import javax.persistence.EntityExistsException;
+import javax.persistence.EntityNotFoundException;
+import java.util.Collections;
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final ModelMapper modelMapper;
+    private final JWTUtil jwtUtil;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder, ModelMapper modelMapper, JWTUtil jwtUtil) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+        this.modelMapper = modelMapper;
+        this.jwtUtil = jwtUtil;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+
+        Optional<User> userRes = userRepository.findByUsername(username);
+
+        if (userRes.isEmpty()) {
+            throw new UsernameNotFoundException(String.format("User with username: %s not found!", username));
+        }
+
+        User user = userRes.get();
+
+        return new org.springframework
+                .security
+                .core
+                .userdetails
+                .User(username, user.getPassword(), Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
     }
 
     @Override
     public void signUp(UserBindingModel userBindingModel) {
 
+        userRepository.findByUsername(userBindingModel.getUsername()).ifPresent(user1 -> {
+            throw new EntityExistsException(String.format("User with username: %s already exist!", userBindingModel.getUsername()));
+        });
+
+        User user = modelMapper.map(userBindingModel, User.class);
+        user.setPassword(bCryptPasswordEncoder.encode(userBindingModel.getPassword()));
+
+        userRepository.save(user);
     }
 
     @Override
     public SignInResponseModel signIn(UserBindingModel userBindingModel) {
-        return null;
+
+        User user = userRepository
+                .findByUsername(userBindingModel.getUsername())
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("Could not find User with username: %s", userBindingModel.getUsername())));
+
+        final String token = jwtUtil.generateToken(user.getUsername());
+
+        SignInResponseModel signInResponseModel = new SignInResponseModel();
+        signInResponseModel.setJWToken(token);
+
+        return signInResponseModel;
+    }
+
+    @Override
+    public void changeUsernameById(Long userId, NewUsernameBindingModel newUsernameBindingModel) {
+
+        User user = userRepository
+                .findById(userId)
+                .orElseThrow(() -> new EntityNotFoundException(String.format("User with id: %d not found!", userId)));
+
+        user.setUsername(newUsernameBindingModel.getUsername());
+
+        userRepository.save(user);
     }
 }
