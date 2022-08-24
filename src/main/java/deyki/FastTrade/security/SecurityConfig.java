228d0b670d7babb3ed33be5583d1243e2261706e
deyki <deikioveca@gmail.com>
2022-08-24 17:33:58 +0300

    Ant matcher for bankaccountcontroller

--- src/main/java/deyki/FastTrade/security/SecurityConfig.java
+++ src/main/java/deyki/FastTrade/security/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeHttpRequests()
                 .antMatchers("/api/auth/**").permitAll()
                 .antMatchers("/api/user/**").hasRole("USER")
+                .antMatchers("/api/bankAccount/**").hasRole("USER")
                 .and()
                 .userDetailsService(userService)
                 .exceptionHandling()
