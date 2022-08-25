a430466cf708a6d551eb23dcd99799f0ae32c12b
deyki <deikioveca@gmail.com>
2022-08-25 20:05:28 +0300

    DigitalWalletResponseModel added

---
+++ src/main/java/deyki/FastTrade/domain/responseModels/digitalWallet/DigitalWalletResponseModel.java
@@ -0,0 +1,10 @@
+package deyki.FastTrade.domain.responseModels.digitalWallet;
+
+import lombok.Data;
+
+@Data
+public class DigitalWalletResponseModel {
+
+    private Float balance;
+    private String userOwner;
+}
