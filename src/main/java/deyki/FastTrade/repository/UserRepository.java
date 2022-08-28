3909eda8c4ab3c9f9518581d917ef2da3d7677fa
deyki <deikioveca@gmail.com>
2022-08-28 11:51:07 +0300

    Queries for findByEmail and findByPhone number added

--- src/main/java/deyki/FastTrade/repository/UserRepository.java
+++ src/main/java/deyki/FastTrade/repository/UserRepository.java
@@ -2,6 +2,8 @@ package deyki.FastTrade.repository;
 
 import deyki.FastTrade.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +12,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByUsername(String username);
+    @Query("SELECT u FROM User u WHERE u.userProfileDetails.email = ?1")
+    Optional<User> findByEmail(@Param("email") String email);
+    @Query("SELECT u FROM User u WHERE u.userProfileDetails.phoneNumber = ?1")
+    Optional<User> findByPhoneNumber(@Param("phoneNumber") Integer phoneNumber);
 }
